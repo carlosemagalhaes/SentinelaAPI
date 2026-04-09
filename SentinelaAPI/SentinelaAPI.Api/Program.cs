8a63ef087164c2409c318ca67d25a164242ff13d
Carlos E. Magalhães <carlosedubts@gmail.com>
2026-04-09 20:02:32 -0300

    feat: fix PostgreSQL connection string parsing for Railway

--- SentinelaAPI/SentinelaAPI.Api/Program.cs
+++ SentinelaAPI/SentinelaAPI.Api/Program.cs
@@ -17,8 +17,12 @@ var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
 if (!string.IsNullOrEmpty(databaseUrl))
 {
+    var uri = new Uri(databaseUrl);
+    var userInfo = uri.UserInfo.Split(':');
+    var npgsqlConnection = $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
+
     builder.Services.AddDbContext<AppDbContext>(options =>
-        options.UseNpgsql(databaseUrl));
+        options.UseNpgsql(npgsqlConnection));
 }
 else
 {
